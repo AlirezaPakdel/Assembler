63b56096709ae28ab24c985b164ae746469a2fad
Alireza Pakdel <alirezapakdel8405@gmail.com>
2026-07-19 14:37:35 +0330

    Add symbolTable and Instruction structures

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
+using namespace std;
+
 #include <iostream>
+#include <string>
+
+
+
+
+struct symbolTable {
+    char name[7];
+    int address;
+};
+
+struct Instruction {
+    string label;
+    string opcode;
+    string args[3];
+    int arg_count;
+};
 
 int main() {
 
