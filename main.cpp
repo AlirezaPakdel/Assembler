3cefff891206161412bf38f315d0897d040c5c7d
Alireza Pakdel <alirezapakdel8405@gmail.com>
2026-07-19 14:47:08 +0330

    Add is numeric  and hex16 functions

--- main.cpp
+++ main.cpp
@@ -18,6 +18,34 @@ struct Instruction {
     int arg_count;
 };
 
+bool is_numeric(const string& str) {
+    if (str.empty()) return false;
+    size_t start ;
+    if (str[0] == '-' || str[0] == '+'){
+        start =1;
+    }else{
+        start =0;
+    }
+    if (start == str.length()){
+        return false;
+    }
+    for (size_t i = start; i < str.length(); ++i) {
+        if (!isdigit(static_cast<unsigned char>(str[i]))){
+            return false;
+        }
+    }
+    return true;
+}
+
+void int2hex16(char *lower, int a) {
+    sprintf(lower, "%04X", a & 0xFFFF);
+}
+
+int hex2int(char *hex) {
+    return (int)strtol(hex, NULL, 16);
+}
+
+
 int main() {
 
 
